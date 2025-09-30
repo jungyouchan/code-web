b065f89b4de426e0390d452daa8f504f71e8aea2
yuchan <yuchan613@naver.com>
2025-09-30 15:48:36 +0900

    change fileName

--- index.js
+++ index.js
@@ -12,6 +12,10 @@ const app = express();
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+})
+
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
